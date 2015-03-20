bcf8e502f5ca06cf1615b32f441d4b1f5941c298
Muggi <masterdagon@hotmail.com>
2015-03-20 11:27:16 +0100

    did problem 9

---
+++ Problem8+9.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Muggi on 20-03-2015.
+ */
+var express = require('express');
+var app = express();
+var array = [];
+var player1 = {
+    id : "1",
+    name : "James Rodríguez",
+    country : "Columbia"
+    };
+array.push(player1);
+
+var player2 = {
+    id : "2",
+    name : "Thomas Mueller",
+    country : "?"
+};
+array.push(player2);
+
+var player3 = {
+    id : "3",
+    name : "Messi",
+    country : "?"
+};
+array.push(player3);
+
+var player4 = {
+    id : "4",
+    name : "Neymar",
+    country : "?"
+};
+array.push(player4);
+
+var player5 = {
+    id : "5",
+    name : "van Persie",
+    country : "?"
+};
+array.push(player5);
+
+
+app.get('/allPlayerNames', function(req, res) {
+    res.type('application/json'); // set content-type
+    res.send(array); // send text response
+});
+app.get('/player/:id', function(req, res) {
+    res.type('application/json'); // set content-type
+    res.send(array[req.param("id")-1]); // send text response
+});
+
+app.listen(process.env.PORT || 80);
